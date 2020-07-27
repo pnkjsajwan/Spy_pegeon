d307d458d5591f39a91a0a38d62ce9608c435992
pnkjsajwan <pankz.sajwan@outlook.com>
2020-07-27 20:59:26 +0530

    API done

--- app/src/main/java/com/example/spy_pegeon/util/Constants.kt
+++ app/src/main/java/com/example/spy_pegeon/util/Constants.kt
@@ -3,5 +3,6 @@ package com.example.spy_pegeon.util
 class Constants {
     companion object{
         const val API_KEY = "c322ffbec3df479cbf9f5d238f86471d"
+        const val BASE_URL = "https://newsapi.org"
     }
 }
