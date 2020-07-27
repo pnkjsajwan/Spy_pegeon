7b9bf0e113a77c821d50d48f142bd5ca71a92e4a
pnkjsajwan <pankz.sajwan@outlook.com>
2020-07-27 21:20:55 +0530

    working on articles

--- app/src/main/java/com/example/spy_pegeon/Article.kt
+++ app/src/main/java/com/example/spy_pegeon/Article.kt
@@ -1,6 +1,14 @@
 package com.example.spy_pegeon
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(
+    tableName = "articles"
+)
 data class Article(
+    @PrimaryKey(autoGenerate = true)
+    var id:Int? = null,
     val author: String,
     val content: String,
     val description: String,
---
+++ app/src/main/java/com/example/spy_pegeon/db/ArticleDao.kt
@@ -0,0 +1,18 @@
+package com.example.spy_pegeon.db
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.spy_pegeon.Article
+
+@Dao
+interface ArticleDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun upsert(article: Article): Long
+
+    @Query("SELECT * FROM articles")
+    fun getAllArticles(): LiveData<List<Article>>
+
+    @Delete
+    suspend fun deleteArticle(article: Article)
+}
