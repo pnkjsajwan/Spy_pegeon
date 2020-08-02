a11992a0e50e8fc8cdb992e7b4bc9efe447f3951
pnkjsajwan <pankz.sajwan@outlook.com>
2020-08-02 12:47:10 +0530

    search news

--- app/src/main/java/com/example/spy_pegeon/ui/fragments/BreakingNewsFragment.kt
+++ app/src/main/java/com/example/spy_pegeon/ui/fragments/BreakingNewsFragment.kt
@@ -25,6 +25,7 @@ class BreakingNewsFragment : Fragment(R.layout.fragment_breaking_news) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
         setupRecyclerView()
+
         viewModel.breakingNews.observe(viewLifecycleOwner, Observer { response ->
             when (response) {
                 is Resource.Success -> {
--- app/src/main/java/com/example/spy_pegeon/util/Constants.kt
+++ app/src/main/java/com/example/spy_pegeon/util/Constants.kt
@@ -4,5 +4,6 @@ class Constants {
     companion object{
         const val API_KEY = "c322ffbec3df479cbf9f5d238f86471d"
         const val BASE_URL = "https://newsapi.org"
+        const val SEARCH_NEWS_TIME_DELAY = 500L
     }
 }
