5b924ee2076344ddf931a2f5c878dff9ff2896e1
pnkjsajwan <pankz.sajwan@outlook.com>
2020-08-02 13:32:57 +0530

    article view done

--- app/src/main/java/com/example/spy_pegeon/ui/fragments/ArticleFragment.kt
+++ app/src/main/java/com/example/spy_pegeon/ui/fragments/ArticleFragment.kt
@@ -2,17 +2,26 @@ package com.example.spy_pegeon.ui.fragments
 
 import android.os.Bundle
 import android.view.View
+import android.webkit.WebViewClient
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import com.example.spy_pegeon.R
 import com.example.spy_pegeon.ui.NewsActivity
 import com.example.spy_pegeon.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_article.*
 
 class ArticleFragment : Fragment(R.layout.fragment_article) {
 
     lateinit var viewModel: NewsViewModel
+    val args: ArticleFragmentArgs by navArgs()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        val article = args.article
+        webView.apply {
+            webViewClient = WebViewClient()
+            loadUrl(article.url)
+        }
     }
 }
--- app/src/main/java/com/example/spy_pegeon/ui/fragments/SavedNewsFragment.kt
+++ app/src/main/java/com/example/spy_pegeon/ui/fragments/SavedNewsFragment.kt
@@ -3,16 +3,42 @@ package com.example.spy_pegeon.ui.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.spy_pegeon.R
+import com.example.spy_pegeon.adapters.NewsAdapters
 import com.example.spy_pegeon.ui.NewsActivity
 import com.example.spy_pegeon.ui.NewsViewModel
+import kotlinx.android.synthetic.main.fragment_breaking_news.*
+import kotlinx.android.synthetic.main.fragment_saved_news.*
 
 class SavedNewsFragment : Fragment(R.layout.fragment_saved_news) {
 
     lateinit var viewModel: NewsViewModel
+    lateinit var newsAdapter: NewsAdapters
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         viewModel = (activity as NewsActivity).viewModel
+        setupRecyclerView()
+
+        newsAdapter.setOnItemClickListener {
+            val bundle = Bundle().apply {
+                putSerializable("article", it)
+            }
+            findNavController().navigate(
+                R.id.action_savedNewsFragment_to_articleFragment,
+                bundle
+            )
+        }
+    }
+
+    private fun setupRecyclerView() {
+        newsAdapter = NewsAdapters()
+
+        rvSavedNews.apply {
+            adapter = newsAdapter
+            layoutManager = LinearLayoutManager(activity)
+        }
     }
 }
